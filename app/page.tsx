464dffe88c079ed0fa1686820535365392a63345
PumkingPie <vera27351@gmail.com>
2025-07-17 16:42:06 -0400

    cambio de posicion de componente: skills.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div>
         <Hero />
         <About />
-        <Portfolio />
         <Skills />
+        <Portfolio />
         <Contact />
       </div>
     </main>
